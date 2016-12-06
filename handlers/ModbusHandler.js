0f86848da6cf38b79ef913fc11f67af58d31685c
Luiz Fernando <lfernando-silva@hotmail.com>
2016-12-05 23:33:06 -0200

    Add files via upload

---
+++ handlers/ModbusHandler.js
@@ -0,0 +1,23 @@
+﻿var Modbus = require('jsmodbus');
+var Promise = require('bluebird');
+
+module.exports = {
+    send: function (value) {
+        
+        var client = Modbus.client.tcp.complete({
+            'host'              : '192.168.0.180', 
+            'port'              : 502,
+            'autoReconnect'     : true,
+            'reconnectTimeout'  : 1000,
+            'timeout'           : 5000,
+            'unitId'            : 0
+        });
+        
+        client.connect();
+        client.on('connect', function () {
+            return client.writeSingleRegister(321, value).then(function (resp) {
+                return resp;
+            }, console.error);
+        });
+    }
+};
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+﻿var express = require('express');
+var router = express.Router();
+var ModbusHandler = require('../handlers/ModbusHandler.js');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/send', function (req, res){
+    var n = Math.floor(Math.random() * 100) + 1 
+
+    var resp = ModbusHandler.send(n);
+    return res.render('index', Boolean(resp));
+})
+
+module.exports = router;
